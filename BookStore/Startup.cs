c9f22c1e981da1a772abb394274d13d8651f2bf6
NVV <uniondirfolder@gmail.com>
2021-02-10 15:38:06 -0800

    Add DbContext

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore
 {
@@ -23,6 +25,10 @@ namespace BookStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
         }
 
