9bc22c306400fdb8fa7103a4ec0a85dea5558458
NVV <uniondirfolder@gmail.com>
2021-02-10 16:36:37 -0800

    do - ctor & bind contx

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -3,14 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Data;
+using BookStore.Models;
 
 namespace BookStore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList = _db.Categories;
+            return View(objList);
         }
     }
 }
