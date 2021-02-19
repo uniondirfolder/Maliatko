b2e2175730e709619c415bbaf6ce1ae563527d3f
NVV <uniondirfolder@gmail.com>
2021-02-18 23:43:54 -0800

    Add User Session Get Method

--- BookStore/Utility/SessionExtensions.cs
+++ BookStore/Utility/SessionExtensions.cs
@@ -7,7 +7,13 @@ namespace BookStore.Utility
     {
         public static void Set<T>(this ISession session, string key, T value) 
         {
-            session.SetString(key, JsonSerializer.Serialize(value));
+            session.SetString(key, JsonSerializer.Serialize(value));           
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default : JsonSerializer.Deserialize<T>(value);
         }
     }
 }
--- BookStore/WC.cs
+++ BookStore/WC.cs
@@ -8,5 +8,6 @@ namespace BookStore
     public static class WC
     {
         public static string ImagePath = @"\images\product\";
+        public static string SessionCart = "ShoppingCartSession"; 
     }
 }
