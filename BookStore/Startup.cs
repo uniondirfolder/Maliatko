14fb3606e5276f5baf56261ef41118dbd75cba91
NVV <uniondirfolder@gmail.com>
2021-02-19 02:09:09 -0800

    Scaffold Adentity Pages

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -60,6 +60,7 @@ namespace BookStore
             app.UseSession();
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
