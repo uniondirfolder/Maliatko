9177b98a32791231afaf90d9fa7245da62241ae3
NVV <uniondirfolder@gmail.com>
2021-02-21 09:44:27 -0800

    Inquiry js

---
+++ BookStore/Controllers/InquiryController.cs
@@ -0,0 +1,38 @@
+﻿using BookStore_DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inquiryHeaderRepository;
+        private readonly IInquiryDetailRepository _inquiryDetailRepository;
+        public InquiryController(IInquiryHeaderRepository inquiryHeaderRepository, 
+            IInquiryDetailRepository inquiryDetailRepository)
+        {
+            _inquiryHeaderRepository = inquiryHeaderRepository;
+            _inquiryDetailRepository = inquiryDetailRepository;
+        }
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetInquiryList() 
+        {
+            return Json(new { data = _inquiryHeaderRepository.GetAll() });
+        }
+
+        #endregion
+    }
+}
---
+++ BookStore_Models/ViewModels/InquiryVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BookStore_Models.ViewModels
+{
+    public class InquiryVM
+    {
+        public InquiryHeader InquiryHeader { get; set; }
+        public IEnumerable<InquiryDetail> InquiryDetails { get; set; }
+    }
+}
