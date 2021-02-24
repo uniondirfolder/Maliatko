829b6aad51b94c0afb4a01570b42d32758e7fa52
NVV <uniondirfolder@gmail.com>
2021-02-23 23:39:42 -0800

    Order List View Model

---
+++ BookStore/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using BookStore_DataAccess.Repository.IRepository;
+using BookStore_Utility.BrainTree;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHeaderRepo;
+        private readonly IOrderDetailRepository _orderDetailRepo;
+        private readonly IBrainTreeGate _brain;
+
+        public OrderController(
+            IOrderHeaderRepository orderHeaderRepo,
+            IOrderDetailRepository orderDetailRepo,
+            IBrainTreeGate brain)
+        {
+            _orderHeaderRepo = orderHeaderRepo;
+            _orderDetailRepo = orderDetailRepo;
+            _brain = brain;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
